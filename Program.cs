9333507bd4daf2eb37117bc762298f44398591c9
Linfantee <145344308+Linfantee@users.noreply.github.com>
2024-02-12 12:03:11 +0100

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Ej3.fechas;
+ 
+
+class Program
+{
+
+    public static void Main(string[] args)
+    {
+
+        Random numeroRamdom = new Random();
+
+        DateTime fecha = DateTime.Now;
+        Console.WriteLine("Fecha hoy");
+        Console.WriteLine(fecha);
+        int numeroRestaAños = numeroRamdom.Next(0, 4);
+        DateTime fechaModificada = fecha.AddDays(1).AddMonths(-24).AddYears(-numeroRestaAños);
+        Console.WriteLine("Resultado");
+        Console.WriteLine(fechaModificada);
+        Console.WriteLine("Comparación de fechas");
+        int comparFechas = fechaModificada.CompareTo(fecha);
+        if (comparFechas > 0)
+        {
+            Console.WriteLine("El hoy es mas pequeño");
+        }
+        else
+        {
+            Console.WriteLine("El hoy es mas grande");
+        }
+
+
+
+
+
+
+    }
+
+
+
+}
